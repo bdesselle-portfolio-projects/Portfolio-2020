5d1e5b20b6dd5fdfbc7fd1d628af31259d7a50cb
[Brandon] <desselle.brandon2@gmail.com>
2020-05-06 23:40:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 // Components
 import Main from './components/Main'
+import Aside from './components/Aside'
+// import Navigation from './components/Navigation'
 
 export default function App() {
   return (
-    <React.Fragment>
-      <Main/>
-    </React.Fragment>
+    <Container>
+      <Row>
+        <Col xs lg='3'><Aside /></Col>
+        <Col><Main /></Col>
+        <Col xs lg='1'><h1>Nav</h1></Col>
+      </Row>
+    </Container>
   )
 }
