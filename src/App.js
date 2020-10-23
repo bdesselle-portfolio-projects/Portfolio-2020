9d82e4d913a791d175a759c3409297759f5bfd7e
BDesselle <desselle.brandon2@gmail.com>
2020-10-23 18:53:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
+// import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 class App extends Component {
